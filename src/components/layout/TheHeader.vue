041f18b2db762329df29ddc3d69b10f1e7f739ac
Diego Reyes <diegorele13@gmail.com>
2023-06-19 18:40:39 -0500

    create: components file

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+	<div class="header">
+		<img src="../../assets/construccionesyacero-logo.png" alt="" />
+		<button>hola</button>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	@import "/src/styles/variables.scss";
+	.header {
+		padding: 12px 32px;
+		background-color: $black;
+		display: flex;
+		justify-content: space-between;
+	}
+</style>

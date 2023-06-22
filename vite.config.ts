a5c7ca83c6653a5a9bc0ce1caf3b3b8073b83be3
Diego Reyes <diegorele13@gmail.com>
2023-06-22 18:44:44 -0500

    set: shortcut to indicate /src

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+          @import '@fortawesome/fontawesome-free/scss/solid.scss';
+          @import '@fortawesome/fontawesome-free/scss/regular.scss';
+          @import '@fortawesome/fontawesome-free/scss/brands.scss';
+          @import '@nabux-crush/crush-styles/nabuxInternalVariables';
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@':  '/src'
+    }
+  }
 })

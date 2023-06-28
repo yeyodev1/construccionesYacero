57add5dcbd50c1ed71f490a9d0cf54db6d4b9ed5
Diego Reyes <diegorele13@gmail.com>
2023-06-28 15:56:43 -0500

    done: services

--- src/components/TheServices.vue
+++ src/components/TheServices.vue
@@ -11,7 +11,6 @@ import ServicesContent from '@/components/services.content.vue'
     <div class="section-detail">
       <p class="section-detail-p">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae maxime aliquam commodi provident impedit unde vitae autem. Aperiam laboriosam, nostrum minus tempore animi quam possimus, eaque voluptatem totam dicta nam.
-        Eaque illum similique numquam praesentium provident suscipit accusantium fugit vel natus in alias quod, necessitatibus nemo repudiandae tenetur veniam iure molestiae explicabo cumque qui debitis aspernatur quos dolores. Alias, repudiandae!
       </p>
       <div class="section-detail-container">
         <img 
@@ -36,6 +35,9 @@ import ServicesContent from '@/components/services.content.vue'
 </template>
 
 <style lang="scss" scoped>
+// general variables
+$break-tablet: 820px;
+
 .section {
   padding: 20px;
   @media (min-width: $tablet-lower-breakpoint) {
@@ -47,22 +49,28 @@ import ServicesContent from '@/components/services.content.vue'
     @media (min-width: $tablet-lower-breakpoint) {
       font-size: 4rem;
     }
+    hyphens: auto;
+    word-break: break-word; 
     margin: 20px 0;
-    width: 100%;
+    text-transform: uppercase;
     span {
       color: $yellow;
     }
   }
   &-detail {
     width: 100%;
+    max-width: $desktop-lower-breakpoint;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 24px;
     @media (min-width: $tablet-lower-breakpoint) {
-      flex-direction: row;
+      align-items: center;
     }
     justify-content: space-around;
     align-items: center;
     margin-bottom: 40px;
+    margin: 0 auto;
     &-p{
       font-size: 1rem;
       margin-bottom: 24px;
@@ -72,33 +80,37 @@ import ServicesContent from '@/components/services.content.vue'
       }
     }
     &-container {
-      height: 300px;
       display: grid;
       grid-template-columns: 1fr 1fr; 
       grid-template-rows: 1fr 1fr; 
       gap: 12px;
+      margin-bottom: 40px;
       @media (min-width: $tablet-lower-breakpoint) {
-      max-width: 40%;
+      max-width: 50%;
       }
       .imagen1 {
         grid-column: 1 / 2; 
         grid-row: 1 / 2;
-        height: 150px;
-        width: 130px;
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        object-position: top center;
       }
       .imagen2 {
         grid-column: 2 / 3; 
         grid-row: 1 / 3;
         height: 100%;
-        width: 130px; 
+        width: 100%; 
         object-fit: cover;
         object-position: top center;
       }
       .imagen3 {
         grid-column: 1 / 2; 
         grid-row: 2 / 3;
-        height: 150px;
-        width: 130px;
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        object-position: top center;
       }
     }
   }

2c4abde5ff970ee396f875759ef4c1416353e7a0
Diego Reyes <diegorele13@gmail.com>
2023-06-28 18:56:55 -0500

    set: call to action

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -1,12 +1,23 @@
 <script setup>
+import CallToAction from '@/components/CallToAction.vue';
 import TheHero from '@/components/TheHero.vue'
 import TheServices from '@/components/TheServices.vue'
+
+const details = [
+  {
+    title: 'tienes un proyecto en',
+    word: 'mente',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique',
+    button: 'cotizar proyecto'
+  }
+]
 </script>
 
 <template>
   <div>
     <TheHero/>
     <TheServices/>
+    <CallToAction :details="details" />
   </div>
 </template>
 

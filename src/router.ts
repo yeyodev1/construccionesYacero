ae9370b33816d6a436bb5e7ca314e4fb39ae89ea
Diego Reyes <diegorele13@gmail.com>
2023-06-29 18:53:59 -0500

    set: about path

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: () => import ('./pages/Home/TheHome.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import ('./pages/About/TheAbout.vue')
   }
 ]
 

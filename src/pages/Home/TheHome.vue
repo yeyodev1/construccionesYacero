9b0cce208cd525b43453d7aa4021c31ed6020ae0
Diego Reyes <diegorele13@gmail.com>
2023-07-03 19:16:57 -0500

    set: call to action phrase

--- src/pages/Home/TheHome.vue
+++ src/pages/Home/TheHome.vue
@@ -8,7 +8,7 @@ const details = [
   {
     title: 'tienes un proyecto en',
     word: 'mente',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique',
+    description: 'Construcciones y Acero lo hace posible. Diseños innovadores, calidad excepcional y resultados impresionantes. Confía en nosotros para convertir tu visión en realidad.',
     button: 'cotizar proyecto'
   }
 ]
@@ -16,7 +16,7 @@ const details2 = [
   {
     title: 'Nos ponemos en',
     word: 'contacto',
-    description: 'Cotiza tu proyecto o pongámonos en contacto, nos encantaría conversar contigo y hacer tu proyecto realidad.',
+    description: 'En Construcciones y Acero, estamos listos para escuchar tus necesidades y brindarte soluciones a medida. Nuestro equipo técnico altamente capacitado y nuestro compromiso con la calidad nos distinguen. Juntos, haremos realidad tus proyectos constructivos.',
     button: 'Conversemos'
   }
 ]

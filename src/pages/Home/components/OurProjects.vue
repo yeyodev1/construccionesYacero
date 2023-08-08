aa9e2cb929df2af5960add98b3b03d24bd590e05
Diego Reyes <diegorele13@gmail.com>
2023-08-08 15:06:16 -0500

    fix: heigh

--- src/pages/Home/components/OurProjects.vue
+++ src/pages/Home/components/OurProjects.vue
@@ -84,6 +84,9 @@ const projectStore = useProjectStore();
       }
       img {
         width: 100%;
+        height: 170px;
+        object-fit: cover;
+        border-radius: 8px;
         @media(min-width: $mobile-upper-breakpoint) {
           width: 250px;
         }
